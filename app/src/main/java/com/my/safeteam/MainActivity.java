fd5d952002d6175bc27dd2a0ccabb81b0201fdcc
Carlos Trujillo <solrakofloda@gmail.com>
2019-09-03 18:49:30 -0400

    Drawer con logeo en google

--- app/src/main/java/com/my/safeteam/MainActivity.java
+++ app/src/main/java/com/my/safeteam/MainActivity.java
@@ -4,41 +4,68 @@ import android.os.Bundle;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.material.navigation.NavigationView;
+import com.my.safeteam.DB.User;
 
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //NAVBAR INITALITATION
     public Toolbar toolbar;
-
     public DrawerLayout drawerLayout;
-
     public NavController navController;
-
     public NavigationView navigationView;
 
+    //GOOGLE THINGS
+    GoogleSignInOptions gso;
+    GoogleSignInClient mGoogleSignInClient;
+    GoogleSignInAccount account;
+    ImageView avatar;
+    TextView displayName;
+    TextView displayEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         setupNavigation();
+        getinGoogleUser();
+
     }
+
+    private void getinGoogleUser(){
+        View hView = navigationView.getHeaderView(0);
+        account = GoogleSignIn.getLastSignedInAccount(MainActivity.this);
+        User user = new User(account.getId(),account.getDisplayName(), account.getPhotoUrl(),account.getEmail());
+        System.out.println(user.toString());
+        avatar = hView.findViewById(R.id.userPhotoAvatar);
+        Glide.with(this).load(user.getPhotoUri()).apply(RequestOptions.circleCropTransform()).into(avatar);
+        displayName = hView.findViewById(R.id.userNameDisplay);
+        displayName.setText(user.getName());
+        displayEmail = hView.findViewById(R.id.userEmailDisplay);
+        displayEmail.setText(user.getEmail());
+    }
+
     //Un cambio random para el commit
     // Setting Up One Time Navigation
     private void setupNavigation() {
         drawerLayout = findViewById(R.id.drawer_layout);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         navigationView = findViewById(R.id.navigationView);
@@ -48,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
 
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
 
     }
 
